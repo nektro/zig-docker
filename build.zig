2ec745d49fc7b538c51eeea3f5ebcfef72f3681a
Meghan Denny <hello@nektro.net>
2022-03-07 02:22:19 -0800

    only build generator program if `run` step is called

--- build.zig
+++ build.zig
@@ -10,13 +10,9 @@ pub fn build(b: *std.build.Builder) void {
     exe.setTarget(target);
     exe.setBuildMode(mode);
     deps.addAllTo(exe);
-    exe.install();
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
